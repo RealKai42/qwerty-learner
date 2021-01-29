044099974060090c9d4c67e5a16917dc37ebb3cc
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2021-01-29 21:45:34 +0800

    feat(Footer): add Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <div className="w-full text-sm text-center pb-1 ease-in">
+      <a className="text-gray-500 no-underline hover:no-underline" href="#/">
+        @ Qwerty Learner
+      </a>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import TypingPage from './typingPage'
+import Footer from '../components/Footer'
 
 const App: React.FC = () => {
   return (
-    <div className="h-screen pb-14 flex flex-col items-center">
+    <div className="h-screen pb-4 flex flex-col items-center">
       <Header />
       <Main>
         <TypingPage />
       </Main>
+      <Footer />
     </div>
   )
 }
