ecd40216dcf3622f35ee8ec66fcde208d4c2d23f
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2021-02-01 16:04:54 +0800

    feat(Info): add github info and email

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer: React.FC = () => {
   return (
     <div className="w-full text-sm text-center pb-1 ease-in">
+      <a href="https://github.com/Kaiyiwing/qwerty-learner">
+        <FontAwesomeIcon icon={['fab', 'github']} className="text-gray-500 mr-3" />
+      </a>
+      <a href="mailto:ZHANG.Kaiyi42@gmail.com">
+        <FontAwesomeIcon icon={['fas', 'envelope']} className="text-gray-500 mr-3" />
+      </a>
+
       <a className="text-gray-500 no-underline hover:no-underline" href="#/">
         @ Qwerty Learner
       </a>
--- src/icon.ts
+++ src/icon.ts
@@ -1,5 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faKeyboard } from '@fortawesome/free-regular-svg-icons'
-import { faBookReader } from '@fortawesome/free-solid-svg-icons'
+import { faBookReader, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faKeyboard, faBookReader)
+library.add(faKeyboard, faBookReader, faGithub, faEnvelope)
