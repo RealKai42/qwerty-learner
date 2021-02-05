c471b730c301b4f8d1dd7be2d82047674b7bc91d
dingsheng <727040141@qq.com>
2021-02-06 00:11:36 +0800

    chore: delete console.log

--- src/components/Translation/index.tsx
+++ src/components/Translation/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Translation: React.FC<TranslationProps> = ({ trans }) => {
-  console.log(trans)
   return <div className="pt-5 pb-4 text-lg font-sans text-center">{trans}</div>
 }
 
