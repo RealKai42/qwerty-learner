8a433f5cb36d5f42895278c6b9b389b092ee28d2
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2021-02-10 13:57:35 +0800

    fix(Header): 修改 package.json 对 endOfLine 的处理，增加 Header Padding避免出现滚动条

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header: React.FC = ({ children }) => {
   return (
-    <nav className="w-full container mx-auto px-0 py-6">
+    <nav className="w-full container mx-auto px-10 py-6">
       <div className="w-full flex items-center justify-between">
         <a className="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#/">
           <FontAwesomeIcon icon={['far', 'keyboard']} className="h-10 fill-current text-indigo-700 pr-2" />
