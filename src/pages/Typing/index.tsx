169c753d25e70fcccdbae4b4f9ec872069677f43
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2021-03-28 17:09:56 +0800

    feat: 修改对移动端用户的提示方式为 alert

--- src/pages/Typing/index.tsx
+++ src/pages/Typing/index.tsx
@@ -18,7 +18,6 @@ import Layout from '../../components/Layout'
 import { NavLink } from 'react-router-dom'
 import usePronunciation from './hooks/usePronunciation'
 import Tooltip from 'components/Tooltip'
-import InfoPanel from 'components/InfoPanel'
 
 const App: React.FC = () => {
   const [order, setOrder] = useState<number>(0)
@@ -30,8 +29,6 @@ const App: React.FC = () => {
   const [switcherState, switcherStateDispatch] = useSwitcherState({ wordVisible: true, phonetic: false })
   const wordList = useWordList()
   const [pronunciation, pronunciationDispatch] = usePronunciation()
-
-  const [showDeviceInfo, setShowDeviceInfo] = useState(false)
 
   const {
     modalState,
@@ -53,7 +50,11 @@ const App: React.FC = () => {
   useEffect(() => {
     // 检测用户设备
     if (!IsDesktop()) {
-      setShowDeviceInfo(true)
+      setTimeout(() => {
+        alert(
+          ' Qwerty Learner 目的为提高键盘工作者的英语输入效率，目前暂未适配移动端，希望您使用桌面端浏览器访问。如您使用的是 Ipad 等平板电脑设备，可以使用外接键盘使用本软件。',
+        )
+      }, 500)
     }
   }, [])
 
@@ -211,29 +212,6 @@ const App: React.FC = () => {
             </div>
           </Main>
         </Layout>
-      )}
-      {showDeviceInfo && (
-        <InfoPanel
-          state={showDeviceInfo}
-          icon="exclamation"
-          color="bg-red-300"
-          btnColor="bg-red-400"
-          iconColor="text-red-500"
-          buttonOnclick={() => setShowDeviceInfo(!showDeviceInfo)}
-        >
-          <>
-            <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-white dark:text-opacity-70" id="modal-headline">
-              暂未适配移动端
-            </h3>
-            <div className="mt-2 ">
-              <br />
-              <p className="text-sm text-gray-500 dark:text-gray-400">
-                Qwerty Learner 目的为提高键盘工作者的英语输入效率，目前暂未适配移动端，希望您使用桌面端浏览器访问。如您使用的是 Ipad
-                等平板电脑设备，可以使用外接键盘使用本软件。
-              </p>
-            </div>
-          </>
-        </InfoPanel>
       )}
     </>
   )
