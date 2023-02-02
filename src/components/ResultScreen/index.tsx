28db97acae6ba23f370188f1ba7b168dbcbfbbec
KaiyiWing <Zhang.kaiyi42@gmail.com>
2023-02-02 16:48:54 +0800

    style: 修改 result screen 样式

--- src/components/ResultScreen/index.tsx
+++ src/components/ResultScreen/index.tsx
@@ -80,29 +80,36 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
   } //check if it is the last chapter
 
   const conclusion = () => {
+    let content
     switch (mistakeLevel) {
       case 0:
-        return (
+        content = (
           <>
             <FontAwesomeIcon icon={['fas', 'heart']} className="text-indigo-600 pt-2" size="lg" />
-            <div className="font-semibold text-lg ml-2 pt-1">表现不错！只错了 {incorrectWords.length} 个单词</div>
+            <span className="text-base font-medium text-gray-700 ml-2 pt-1">表现不错！只错了 {incorrectWords.length} 个单词</span>
           </>
         )
+        break
       case 1:
-        return (
-          <>
-            <FontAwesomeIcon icon={['fas', 'thumbs-up']} className="text-indigo-600 pt-2" size="lg" />
-            <div className="font-semibold text-lg ml-2 pt-1">有些小问题哦，下一次可以做得更好！</div>
-          </>
+        content = (
+          <div className="">
+            <FontAwesomeIcon icon={['fas', 'thumbs-up']} className="text-indigo-600 leading-10 " size="lg" />
+            <div className="text-base font-medium text-gray-700 ml-2 leading-10 inline-block align-middle">
+              有些小问题哦，下一次可以做得更好！
+            </div>
+          </div>
         )
+        break
       case 2:
-        return (
+        content = (
           <>
             <FontAwesomeIcon icon={['fas', 'exclamation-triangle']} className="text-indigo-600 pt-2" size="lg" />
-            <div className="font-semibold text-lg ml-2 pt-1">错误太多，再来一次如何？</div>
+            <div className="text-base font-medium text-gray-700 ml-2 pt-1">错误太多，再来一次如何？</div>
           </>
         )
+        break
     }
+    return <div className="h-10">{content}</div>
   }
 
   const disabledClassName: string = lastChapter() ? 'cursor-not-allowed opacity-50' : ''
@@ -138,7 +145,9 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
       >
         <div className="flex items-center justify-center h-screen">
           <div className="w-3/5 h-2/3 card bg-white dark:bg-gray-800 rounded-3xl shadow-lg fixed flex flex-col overflow-hidden">
-            <div className="text-center mt-10 font-sans font-semibold text-2xl dark:text-white">{dictNameCombined}</div>
+            <div className="text-center mt-10 text-base font-sans font-normal text-gray-700 text-2xl dark:text-white">
+              {dictNameCombined}
+            </div>
 
             <div className="flex flex-row gap-2 mt-10 overflow-hidden mx-10">
               <div className="flex flex-col gap-3 flex-grow-0 w-40 px-4">
@@ -153,10 +162,10 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
                       cx="3.5rem"
                       cy="3.5rem"
                     />
-                    <text x="3.5rem" y="3.2rem" textAnchor="middle" dominantBaseline="middle" fontSize="2rem">
+                    <text x="3.5rem" y="3.2rem" textAnchor="middle" dominantBaseline="middle" fontSize="1.6rem">
                       {correctRate}%
                     </text>
-                    <text x="3.5rem" y="4.5rem" textAnchor="middle" dominantBaseline="middle" fontSize="1rem">
+                    <text x="3.5rem" y="4.5rem" textAnchor="middle" dominantBaseline="middle" fontSize="0.95rem">
                       正确率
                     </text>
                     <circle
@@ -184,10 +193,10 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
                       cx="3.5rem"
                       cy="3.5rem"
                     />
-                    <text x="3.5rem" y="3.25rem" textAnchor="middle" dominantBaseline="middle" fontSize="1.5rem">
+                    <text x="3.5rem" y="3.25rem" textAnchor="middle" dominantBaseline="middle" fontSize="1.2rem">
                       {timeString}
                     </text>
-                    <text x="3.5rem" y="4.75rem" textAnchor="middle" dominantBaseline="middle" fontSize="1rem">
+                    <text x="3.5rem" y="4.75rem" textAnchor="middle" dominantBaseline="middle" fontSize="0.95rem">
                       章节耗时
                     </text>
                   </svg>
@@ -203,17 +212,17 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
                       cx="3.5rem"
                       cy="3.5rem"
                     />
-                    <text x="3.5rem" y="3.25rem" textAnchor="middle" dominantBaseline="middle" fontSize="1.5rem">
+                    <text x="3.5rem" y="3.25rem" textAnchor="middle" dominantBaseline="middle" fontSize="1.2rem">
                       {speed}个/s
                     </text>
-                    <text x="3.5rem" y="4.75rem" textAnchor="middle" dominantBaseline="middle" fontSize="1rem">
+                    <text x="3.5rem" y="4.75rem" textAnchor="middle" dominantBaseline="middle" fontSize="0.95rem">
                       输入字符
                     </text>
                   </svg>
                 </div>
               </div>
-              <div className="rounded-xl bg-indigo-50 flex-grow mx-6 overflow-hidden">
-                <div className="flex flex-row gap-4 flex-wrap overflow-y-auto overflow-x-hidden customized-scrollbar h-80 content-start ml-8 mr-1 pr-7 pt-9">
+              <div className="rounded-xl bg-indigo-50 flex-grow mx-6 overflow-visible z-10">
+                <div className="flex flex-row gap-4 flex-wrap overflow-y-auto overflow-x-hidden customized-scrollbar h-80 content-start ml-8 mr-1 pr-7 pt-9 z-20">
                   {incorrectWords.map((word, index) => {
                     return (
                       <Tooltip content={`${incorrectTranslations[index]}`}>
@@ -222,13 +231,13 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
                             collectList[index] ? 'border-indigo-600' : 'border-indigo-300'
                           } border-solid border-2 rounded-md bg-white hover:bg-indigo-100 w-auto h-12 px-5 py-1 flex flex-row gap-3 cursor-pointer transition-colors duration-100`}
                         >
-                          <div className="font-mono text-3xl">{word}</div>
+                          <div className="font-mono font-light text-gray-600 text-3xl">{word}</div>
                         </div>
                       </Tooltip>
                     )
                   })}
                 </div>
-                <div className="bg-indigo-200 w-full h-10 rounded-b-lg flex flex-row px-4">
+                <div className="bg-indigo-200 w-full rounded-b-lg flex flex-row justify-start align-center px-4">
                   <>{conclusion()}</>
                 </div>
               </div>
@@ -237,7 +246,7 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
             <div className="w-full flex justify-center mt-10 gap-5 px-5 text-xl">
               <Tooltip content="快捷键：shift + enter">
                 <button
-                  className="rounded-md bg-white hover:bg-indigo-200 px-6 py-2 font-semibold transition-colors duration-100 border-solid border-2 border-indigo-400"
+                  className="rounded-md bg-white hover:bg-indigo-200 px-6 py-2 h-12 text-base font-normal text-gray-700 transition-colors duration-100 border-solid border-2 border-gray-300"
                   onClick={invisibleButtonHandler}
                 >
                   默写本章节
@@ -245,7 +254,7 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
               </Tooltip>
               <Tooltip content="快捷键：space">
                 <button
-                  className="rounded-md bg-white hover:bg-indigo-200 px-6 py-2 font-semibold transition-colors duration-100 border-solid border-2 border-indigo-400 "
+                  className="rounded-md bg-white hover:bg-indigo-200 px-6 py-2 h-12 text-base font-normal text-gray-700 transition-colors duration-100 border-solid border-2 border-gray-300 "
                   onClick={repeatButtonHandler}
                 >
                   重复本章节
@@ -253,7 +262,7 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
               </Tooltip>
               <Tooltip content="快捷键：enter">
                 <button
-                  className={`rounded-md bg-indigo-400 hover:bg-indigo-600 px-6 py-2 font-semibold transition-colors duration-100 ${disabledClassName}`}
+                  className={`rounded-md bg-indigo-400 hover:bg-indigo-600 px-6 py-2 h-12 text-base font-bold text-white transition-colors duration-100 ${disabledClassName}`}
                   onClick={nextButtonHandler}
                   disabled={lastChapter()}
                 >
