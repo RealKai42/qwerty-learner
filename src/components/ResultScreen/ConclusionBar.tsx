5a3cc3c455d30f663d28f214bca805f6114fd69e
Luyu Cheng <chengluyu@live.cn>
2023-02-04 20:44:58 +0800

    fix: align the icon and the text in the conclusion bar

--- src/components/ResultScreen/ConclusionBar.tsx
+++ src/components/ResultScreen/ConclusionBar.tsx
@@ -6,7 +6,7 @@ const ConclusionBar = ({ mistakeLevel, mistakeCount }: ConclusionBarProps) => {
     case 0:
       content = (
         <>
-          <FontAwesomeIcon icon={['fas', 'heart']} className="text-indigo-600 pt-2" size="lg" />
+          <FontAwesomeIcon icon={['fas', 'heart']} className="text-indigo-600" size="lg" />
           <span className="text-base font-medium text-gray-700 ml-2 leading-10 inline-block align-middle">
             表现不错！只错了 {mistakeCount} 个单词
           </span>
@@ -15,24 +15,24 @@ const ConclusionBar = ({ mistakeLevel, mistakeCount }: ConclusionBarProps) => {
       break
     case 1:
       content = (
-        <div className="">
-          <FontAwesomeIcon icon={['fas', 'thumbs-up']} className="text-indigo-600 leading-10 " size="lg" />
+        <>
+          <FontAwesomeIcon icon={['fas', 'thumbs-up']} className="text-indigo-600 leading-10" size="lg" />
           <div className="text-base font-medium text-gray-700 ml-2 leading-10 inline-block align-middle">
             有些小问题哦，下一次可以做得更好！
           </div>
-        </div>
+        </>
       )
       break
     case 2:
       content = (
         <>
-          <FontAwesomeIcon icon={['fas', 'exclamation-triangle']} className="text-indigo-600 pt-2" size="lg" />
+          <FontAwesomeIcon icon={['fas', 'exclamation-triangle']} className="text-indigo-600" size="lg" />
           <div className="text-base font-medium text-gray-700 ml-2 leading-10 inline-block align-middle">错误太多，再来一次如何？</div>
         </>
       )
       break
   }
-  return <div className="h-10">{content}</div>
+  return <div className="h-10 flex flex-row items-center">{content}</div>
 }
 
 export type ConclusionBarProps = {
