76a886ad025e6f2bd3c9566a2884699ba4b955a9
Yangthemob <haoyangmob@163.com>
2023-02-06 01:44:05 +0800

    refactor: simplify ConclusionBar

--- src/components/ResultScreen/ConclusionBar.tsx
+++ src/components/ResultScreen/ConclusionBar.tsx
@@ -1,38 +1,37 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+const ICON_MAPPER = [
+  {
+    icon: ['fas', 'heart'],
+    className: 'text-indigo-600',
+    size: 'lg',
+    text: (mistakeCount: number) => `表现不错！只错了 ${mistakeCount} 个单词`,
+  },
+  {
+    icon: ['fas', 'thumbs-up'],
+    className: 'text-indigo-600',
+    size: 'lg',
+    text: () => '有些小问题哦，下一次可以做得更好！',
+  },
+  {
+    icon: ['fas', 'exclamation-triangle'],
+    className: 'text-indigo-600',
+    size: 'lg',
+    text: () => '错误太多，再来一次如何？',
+  },
+]
+
 const ConclusionBar = ({ mistakeLevel, mistakeCount }: ConclusionBarProps) => {
-  let content
-  switch (mistakeLevel) {
-    case 0:
-      content = (
-        <>
-          <FontAwesomeIcon icon={['fas', 'heart']} className="text-indigo-600" size="lg" />
-          <span className="text-base font-medium text-gray-700 ml-2 leading-10 inline-block align-middle">
-            表现不错！只错了 {mistakeCount} 个单词
-          </span>
-        </>
-      )
-      break
-    case 1:
-      content = (
-        <>
-          <FontAwesomeIcon icon={['fas', 'thumbs-up']} className="text-indigo-600 leading-10" size="lg" />
-          <div className="text-base font-medium text-gray-700 ml-2 leading-10 inline-block align-middle">
-            有些小问题哦，下一次可以做得更好！
-          </div>
-        </>
-      )
-      break
-    case 2:
-      content = (
-        <>
-          <FontAwesomeIcon icon={['fas', 'exclamation-triangle']} className="text-indigo-600" size="lg" />
-          <div className="text-base font-medium text-gray-700 ml-2 leading-10 inline-block align-middle">错误太多，再来一次如何？</div>
-        </>
-      )
-      break
-  }
-  return <div className="h-10 flex flex-row items-center">{content}</div>
+  const { icon, className, size, text } = ICON_MAPPER[mistakeLevel]
+
+  return (
+    <div className="h-10 flex flex-row items-center">
+      <FontAwesomeIcon icon={icon as any} className={className} size={size as any} />
+      <span className="md:text-base sm:text-sm text-sm font-medium text-gray-700 ml-2 leading-10 inline-block align-middle">
+        {text(mistakeCount)}
+      </span>
+    </div>
+  )
 }
 
 export type ConclusionBarProps = {
