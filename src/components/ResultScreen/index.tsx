8291275bb5e3b54b5003a6335303de4a4a8f91cd
Yangthemob <haoyangmob@163.com>
2023-02-06 01:46:00 +0800

    fix: fix text overflow and add responsive size

--- src/components/ResultScreen/index.tsx
+++ src/components/ResultScreen/index.tsx
@@ -98,23 +98,23 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
       >
         <div className="flex items-center justify-center h-screen">
           <div className="w-3/5 h-2/3 card bg-white dark:bg-gray-800 rounded-3xl shadow-lg fixed flex flex-col overflow-hidden">
-            <div className="text-center mt-10 font-sans font-normal text-gray-900 sm:text-xl md:text-2xl dark:text-white">
+            <div className="text-center mt-10 font-sans font-normal text-gray-700 text-2xl dark:text-white">
               {wordList ? `${wordList.dictName}  第${wordList.chapter + 1}章` : ' '}
             </div>
 
             <div className="flex flex-row gap-2 mt-10 overflow-hidden mx-10">
-              <div className="flex flex-col gap-3 flex-grow-0 w-40 px-4 lg:px-4 md:px-2 sm:px-1">
+              <div className="flex flex-col gap-3 flex-grow-0 w-40 px-4">
                 <RemarkRing remark={`${correctRate}%`} caption="正确率" percentage={correctRate} />
                 <RemarkRing remark={timeString} caption="章节耗时" />
                 <RemarkRing remark={`${speedInfo.speed}个/s`} caption="输入字符" />
               </div>
-              <div className="rounded-xl bg-indigo-50 dark:bg-gray-700 flex-grow mx-6 overflow-visible z-10">
+              <div className="rounded-xl bg-indigo-50 flex-grow mx-6 overflow-visible z-10">
                 <div className="flex flex-row gap-4 flex-wrap overflow-y-auto overflow-x-hidden customized-scrollbar h-80 content-start ml-8 mr-1 pr-7 pt-9 z-20">
                   {incorrectInfo.map((info) => (
                     <WordChip key={info.word} mistake={info} />
                   ))}
                 </div>
-                <div className="bg-indigo-200 dark:bg-indigo-400 w-full rounded-b-xl flex flex-row justify-start align-center px-4">
+                <div className="bg-indigo-200 w-full rounded-b-xl flex flex-row justify-start align-center px-4">
                   <ConclusionBar mistakeLevel={mistakeLevel} mistakeCount={incorrectInfo.length} />
                 </div>
               </div>
@@ -123,7 +123,7 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
             <div className="w-full flex justify-center mt-10 gap-5 px-5 text-xl">
               <Tooltip content="快捷键：shift + enter">
                 <button
-                  className="rounded-md overflow-hidden bg-white dark:bg-gray-600 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 hover:bg-indigo-200 px-6 py-2 h-12 md:text-base sm:text-sm text-sm font-normal text-gray-700 transition-colors duration-100 border-solid border-2 border-gray-300"
+                  className="rounded-md bg-white hover:bg-indigo-200 px-6 py-2 h-12 text-base font-normal text-gray-700 transition-colors duration-100 border-solid border-2 border-gray-300"
                   onClick={invisibleButtonHandler}
                 >
                   默写本章节
@@ -131,7 +131,7 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
               </Tooltip>
               <Tooltip content="快捷键：space">
                 <button
-                  className="rounded-md overflow-hidden bg-white dark:bg-gray-600 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 hover:bg-indigo-200 px-6 py-2 h-12 md:text-base sm:text-sm text-sm font-normal text-gray-700 transition-colors duration-100 border-solid border-2 border-gray-300"
+                  className="rounded-md bg-white hover:bg-indigo-200 px-6 py-2 h-12 text-base font-normal text-gray-700 transition-colors duration-100 border-solid border-2 border-gray-300 "
                   onClick={repeatButtonHandler}
                 >
                   重复本章节
@@ -139,7 +139,7 @@ const ResultScreen: React.FC<ResultScreenProps> = ({
               </Tooltip>
               <Tooltip content="快捷键：enter">
                 <button
-                  className={`rounded-md overflow-hidden bg-indigo-400 hover:bg-indigo-600 px-6 py-2 h-12 md:text-base sm:text-sm text-sm font-bold text-white transition-colors duration-100 ${
+                  className={`rounded-md bg-indigo-400 hover:bg-indigo-600 px-6 py-2 h-12 text-base font-bold text-white transition-colors duration-100 ${
                     isLastChapter ? 'cursor-not-allowed opacity-50' : ''
                   }`}
                   onClick={nextButtonHandler}
