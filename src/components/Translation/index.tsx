91a36df2caef36c6abb37b908e3b0f2f59d679d6
Caffeine19 <939597201@qq.com>
2023-02-15 15:14:04 +0800

    fix: 调整译文较长时候的样式

--- src/components/Translation/index.tsx
+++ src/components/Translation/index.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Translation: React.FC<TranslationProps> = ({ trans }) => {
   return (
-    <div className="pt-5 pb-4 text-lg font-sans text-center dark:text-white dark:text-opacity-80 transition-colors duration-300">
-      {trans}
-    </div>
+    <div className="pt-5 pb-4 text-lg font-sans dark:text-white dark:text-opacity-80 transition-colors duration-300 max-w-4xl">{trans}</div>
   )
 }
 
