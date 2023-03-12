a2b76431b642472924adb7fba47f4a7f52b96650
KaiyiWing <Zhang.kaiyi42@gmail.com>
2023-03-12 17:52:54 +0800

    feat: add darkMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './icon'
@@ -9,6 +9,8 @@ import TypingPage from './pages/Typing'
 import mixpanel from 'mixpanel-browser'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
+import { useAtomValue } from 'jotai'
+import { isOpenDarkModeAtom } from '@/store'
 
 mixpanel.init('bdc492847e9340eeebd53cc35f321691')
 dayjs.extend(utc)
@@ -17,13 +19,22 @@ const container = document.getElementById('root')
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!)
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={REACT_APP_DEPLOY_ENV === 'pages' ? '/qwerty-learner' : ''}>
-      <Routes>
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="/" element={<TypingPage />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+function Root() {
+  const darkMode = useAtomValue(isOpenDarkModeAtom)
+  useEffect(() => {
+    darkMode ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
+  }, [darkMode])
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter basename={REACT_APP_DEPLOY_ENV === 'pages' ? '/qwerty-learner' : ''}>
+        <Routes>
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/" element={<TypingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
+
+root.render(<Root />)
