8f88ecf23d759f4eac43425e5022e988fbf7a6fd
Kaiyi <me@kaiyi.cool>
2023-04-10 23:55:00 +0800

    feat: split multi input handler (#377)

--- src/pages/Typing/components/CurrentWord/components/TextAreaHandler/index.tsx
+++ src/pages/Typing/components/CurrentWord/components/TextAreaHandler/index.tsx
@@ -1,14 +1,11 @@
-import { TypingContext, TypingStateActionType } from '@/pages/Typing/store'
+import { TypingContext } from '@/pages/Typing/store'
 import { FormEvent, useCallback, useContext, useEffect, useRef } from 'react'
+import { WordUpdateAction } from '../InputHandler'
 
-export type HiddenTextareaProps = {
-  updateInput: (updateObj: WordUpdateAction) => void
-}
-
-export default function HiddenTextarea({ updateInput }: HiddenTextareaProps) {
+export default function TextAreaHandler({ updateInput }: { updateInput: (updateObj: WordUpdateAction) => void }) {
   const textareaRef = useRef<HTMLTextAreaElement>(null)
   // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
-  const { state, dispatch } = useContext(TypingContext)!
+  const { state } = useContext(TypingContext)!
 
   useEffect(() => {
     if (!textareaRef.current) return
@@ -19,10 +16,6 @@ export default function HiddenTextarea({ updateInput }: HiddenTextareaProps) {
       textareaRef.current.blur()
     }
   }, [state.isTyping])
-
-  const onBlur = useCallback(() => {
-    dispatch({ type: TypingStateActionType.SET_IS_TYPING, payload: false })
-  }, [dispatch])
 
   const onInput = (e: FormEvent<HTMLTextAreaElement>) => {
     const nativeEvent = e.nativeEvent as InputEvent
@@ -35,36 +28,25 @@ export default function HiddenTextarea({ updateInput }: HiddenTextareaProps) {
     }
   }
 
+  const onBlur = useCallback(() => {
+    if (!textareaRef.current) return
+
+    if (state.isTyping) {
+      textareaRef.current.focus()
+    }
+  }, [state.isTyping])
+
   return (
     <textarea
       className="absolute left-0 top-0 m-0 h-0 w-0 appearance-none overflow-hidden border-0 p-0 focus:outline-none"
       ref={textareaRef}
       autoFocus
       spellCheck="false"
-      onBlur={onBlur}
       onInput={onInput}
+      onBlur={onBlur}
       onCompositionStart={() => {
         alert('您正在使用输入法，请关闭输入法。')
       }}
     ></textarea>
   )
-}
-
-export type WordUpdateAction = WordAddAction | WordDeleteAction | WordCompositionAction
-
-export type WordAddAction = {
-  type: 'add'
-  value: string
-  event: FormEvent<HTMLTextAreaElement>
-}
-
-export type WordDeleteAction = {
-  type: 'delete'
-  length: number
-}
-
-// composition api is not ready yet
-export type WordCompositionAction = {
-  type: 'composition'
-  value: string
 }
