eec25e7c36c4537fb446ec14e4a279e6d457390f
Kaiyi <me@kaiyi.cool>
2023-04-11 15:22:12 +0800

    feat: optimized for seo (#380)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,20 +4,20 @@ import { ReactComponent as Logo } from '@/assets/logo.svg'
 
 const Header: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <nav className="container mx-auto w-full px-10 py-6">
+    <header className="container mx-auto w-full px-10 py-6">
       <div className="flex w-full flex-col items-center justify-between space-y-3 lg:flex-row lg:space-y-0">
         <NavLink
           className="flex items-center text-2xl font-bold text-indigo-400 no-underline hover:no-underline lg:text-4xl"
           to="https://qwerty.kaiyi.cool/"
         >
           <Logo className="mr-3 h-16 w-16" />
-          Qwerty Learner
+          <h1>Qwerty Learner</h1>
         </NavLink>
-        <div className="card on element flex w-auto content-center items-center justify-end space-x-3 rounded-large bg-white p-4 transition-colors duration-300 dark:bg-gray-800">
+        <nav className="card on element flex w-auto content-center items-center justify-end space-x-3 rounded-large bg-white p-4 transition-colors duration-300 dark:bg-gray-800">
           {children}
-        </div>
+        </nav>
       </div>
-    </nav>
+    </header>
   )
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Footer from './Footer'
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <div className="flex h-screen w-full flex-col items-center pb-4">
+  <main className="flex h-screen w-full flex-col items-center pb-4">
     {children}
     <Footer />
-  </div>
+  </main>
 )
 
 Layout.displayName = 'Layout'
