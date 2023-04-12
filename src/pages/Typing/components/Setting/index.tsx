c6f3cc8da3ad0f2117dac751bc88e9b38f100de3
KaiyiWing <Zhang.kaiyi42@gmail.com>
2023-04-12 13:13:04 +0800

    chore: polish detail

--- src/pages/Typing/components/Setting/index.tsx
+++ src/pages/Typing/components/Setting/index.tsx
@@ -27,7 +27,7 @@ export default function Setting() {
         type="button"
         onClick={openModal}
         className={`flex items-center justify-center rounded p-[2px] py-[3px] text-lg text-indigo-400 outline-none transition-colors duration-300 ease-in-out hover:bg-indigo-400 hover:text-white  ${
-          isOpen ? 'bg-indigo-400 text-white' : ''
+          isOpen && 'bg-indigo-400 text-white'
         }`}
       >
         <FontAwesomeIcon icon="gear" fixedWidth className="focus:outline-none" />
