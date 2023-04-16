55cd87bd7081e494625ea56ec33eb17df27aad86
Yangthemob <haoyangmob@163.com>
2023-04-17 00:37:28 +0800

    try some styling

--- src/pages/Typing/components/SideWordList/index.tsx
+++ src/pages/Typing/components/SideWordList/index.tsx
@@ -22,7 +22,7 @@ const SideWordList: React.FC<SideWordListProps> = ({ words, index, isVisible, se
   return (
     <div className="fixed left-0 z-10 h-screen">
       <div
-        className={`relative w-52 transform overflow-y-auto rounded-r-2xl bg-indigo-100 p-5 pl-8 shadow-md transition-transform duration-300 dark:bg-indigo-300  ${
+        className={`relative transform overflow-y-auto rounded-r-2xl border-2 border-indigo-400 bg-indigo-50 p-5 pl-8 shadow-md transition-transform duration-300 dark:bg-gray-800  ${
           isVisible ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
@@ -30,8 +30,8 @@ const SideWordList: React.FC<SideWordListProps> = ({ words, index, isVisible, se
           <div
             key={i}
             onClick={() => handleWordClick(i)}
-            className={`cursor-pointer rounded-lg px-2 py-1 transition-all duration-300 ease-in-out hover:bg-white ${
-              i === index ? 'font-bold text-indigo-500' : ''
+            className={`cursor-pointer rounded-lg border-2 border-indigo-50 px-3 py-1 transition-all duration-100 ease-in-out hover:border-indigo-300 hover:bg-white dark:border-gray-800 dark:hover:border-gray-100 dark:hover:bg-indigo-400 ${
+              i === index ? 'font-bold text-indigo-500 dark:text-indigo-500' : 'dark:text-gray-100'
             }`}
           >
             {word.name}
