8d31d7049d425bd3973d61fe69d8de9849bdb45a
Luyu Cheng <chengluyu@live.cn>
2023-04-16 23:57:50 +0800

    feat: show the latest commit hash at the footer (#394)

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,31 @@ import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
 import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
 import path from 'node:path'
 import svgr from 'vite-plugin-svgr'
+import { getLastCommit } from 'git-last-commit'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }), svgr()],
-  build: {
-    outDir: 'build',
-  },
-  define: {
-    REACT_APP_DEPLOY_ENV: JSON.stringify(process.env.REACT_APP_DEPLOY_ENV),
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(async () => {
+  const latestCommitHash = await new Promise<string>((resolve) => {
+    return getLastCommit((err, commit) => (err ? 'unknown' : resolve(commit.shortHash)))
+  })
+  return {
+    plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }), svgr()],
+    build: {
+      outDir: 'build',
     },
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
+    define: {
+      REACT_APP_DEPLOY_ENV: JSON.stringify(process.env.REACT_APP_DEPLOY_ENV),
+      LATEST_COMMIT_HASH: JSON.stringify(latestCommitHash + (process.env.NODE_ENV === 'production' ? '' : ' (dev)')),
     },
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly',
+      },
+    },
+  }
 })
