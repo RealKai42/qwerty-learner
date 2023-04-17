d549ad5b079edae4184c2b8d926fab4c30ad359a
Franky Liu <franky@biteinvestments.com>
2023-04-17 15:36:53 +0800

    fix: reset selected chapter after dict change

--- src/pages/Gallery/ChapterGroup.tsx
+++ src/pages/Gallery/ChapterGroup.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { range } from 'lodash'
-import ChapterButton from './ChapterButton'
-import { useAtom, useAtomValue } from 'jotai'
-import { currentChapterAtom, currentDictInfoAtom } from '@/store'
 import { CHAPTER_LENGTH } from '@/constants'
+import { currentChapterAtom, currentDictInfoAtom } from '@/store'
+import { useAtom, useAtomValue } from 'jotai'
+import { range } from 'lodash'
+import React from 'react'
+import { useUpdateEffect } from 'react-use'
+import ChapterButton from './ChapterButton'
 
 const ChapterGroup: React.FC<ChapterGroupProps> = ({ totalWords }) => {
   const [currentChapter, setCurrentChapter] = useAtom(currentChapterAtom)
   const currentDictInfo = useAtomValue(currentDictInfoAtom)
+
+  useUpdateEffect(() => {
+    setCurrentChapter(0)
+  }, [currentDictInfo, setCurrentChapter])
 
   return (
     <main className="mr-4 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
