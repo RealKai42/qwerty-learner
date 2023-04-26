8a459c66f9ce8c396c3f99d5d8efc59ccb9467ff
mancuoj <1337381768@qq.com>
2023-04-26 21:03:42 +0800

    feat: update result screen

--- src/pages/Typing/components/SoundSwitcher/index.tsx
+++ src/pages/Typing/components/SoundSwitcher/index.tsx
@@ -51,9 +51,7 @@ export default function SoundSwitcher() {
             <Popover.Panel className="absolute left-1/2 z-10 mt-2 flex max-w-max -translate-x-1/2 px-4 ">
               <div className="shadow-upper box-border flex w-60 select-none flex-col items-center justify-center gap-4 rounded-xl bg-white p-4 drop-shadow dark:bg-gray-800">
                 <div className="flex w-full  flex-col  items-start gap-2 py-0">
-                  <span className="text-sm font-medium font-normal leading-5 text-gray-900 dark:text-white dark:text-opacity-60">
-                    开关按键音
-                  </span>
+                  <span className="text-sm font-normal leading-5 text-gray-900 dark:text-white dark:text-opacity-60">开关按键音</span>
                   <div className="flex w-full flex-row items-center justify-between">
                     <Switch checked={keySoundsConfig.isOpen} onChange={onChangeKeySound} className="switch-root">
                       <span aria-hidden="true" className="switch-thumb" />
