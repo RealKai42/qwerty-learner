f562d07ef71da1fd2ce99c4a54ff83bcfbe14c25
KaiyiWing <Zhang.kaiyi42@gmail.com>
2023-04-28 12:28:41 +0800

    style: polish style

--- src/pages/Typing/components/ResultScreen/index.tsx
+++ src/pages/Typing/components/ResultScreen/index.tsx
@@ -154,7 +154,7 @@ const ResultScreen = () => {
 
                 <IconXiaoHongShu
                   fontSize={15}
-                  className="cursor-pointer text-red-500 focus:outline-none"
+                  className="cursor-pointer text-gray-500 hover:text-red-500 focus:outline-none"
                   onClick={(e) => {
                     handleOpenInfoPanel('redBook')
                     e.currentTarget.blur()
@@ -170,7 +170,7 @@ const ResultScreen = () => {
                   type="button"
                   title="捐赠我们的项目"
                 >
-                  <IconCoffee fontSize={17} className={`text-amber-500 focus:outline-none ${styles.imgShake}`} />
+                  <IconCoffee fontSize={17} className={`text-gray-500 hover:text-amber-500  focus:outline-none ${styles.imgShake}`} />
                 </button>
 
                 <button
@@ -182,11 +182,11 @@ const ResultScreen = () => {
                   type="button"
                   title="加入我们的社区"
                 >
-                  <IconWechat fontSize={16} className="text-green-500 focus:outline-none" />
+                  <IconWechat fontSize={16} className="text-gray-500 hover:text-green-500 focus:outline-none" />
                 </button>
 
                 <a href="https://github.com/Kaiyiwing/qwerty-learner" target="_blank" rel="noreferrer" className="leading-[0px]">
-                  <IconGithub fontSize={16} className="text-gray-500 focus:outline-none" />
+                  <IconGithub fontSize={16} className="text-gray-500 hover:text-green-800 focus:outline-none" />
                 </a>
               </div>
             </div>
