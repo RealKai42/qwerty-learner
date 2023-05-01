f2fd2b31400515c7cdfe3af2446cdbfc0466c9e1
邓亮 <787615673@qq.com>
2023-05-01 18:19:24 +0800

    fix: 修复了key 类型问题

--- src/resources/soundResource.ts
+++ src/resources/soundResource.ts
@@ -7,15 +7,20 @@ const videoList = import.meta.glob(['/qwerty-learner/sounds/*.wav', '../../publi
   eager: false,
 })
 
-export const keySoundResources: SoundResource[] = Object.keys(videoList).map((k) => {
-  const name = k.replace(/(.*\/)*([^.]+).*/gi, '$2')
-  const suffix = k.substring(k.lastIndexOf('.'))
-  return {
-    key: name,
-    name: `声音${name}`,
-    filename: `${name}${suffix}`,
-  }
-})
+export const keySoundResources: SoundResource[] = Object.keys(videoList)
+  .map((k) => {
+    const name = k.replace(/(.*\/)*([^.]+).*/gi, '$2')
+    console.log('name: ', name)
+    const suffix = k.substring(k.lastIndexOf('.'))
+    console.log('suffix: ', suffix)
+    console.log('filename: ', `${name}${suffix}`)
+    return {
+      key: name,
+      name: `声音${name}`,
+      filename: `${name}${suffix}`,
+    }
+  })
+  .sort((a: SoundResource, b: SoundResource) => a.key.localeCompare(b.key))
 
 // export const keySoundResources: SoundResource[] = [{ key: '1', name: '声音1', filename: 'click.wav' }]
 
