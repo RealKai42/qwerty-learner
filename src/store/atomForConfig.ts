264c8314539a60692ee7ac24d148881aa6d72efd
Luyu Cheng <chengluyu@live.cn>
2023-05-06 15:00:55 +0800

    refactor(store): add `atomForConfig` which checks on read

---
+++ src/store/atomForConfig.ts
@@ -0,0 +1,22 @@
+import { atom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
+
+export default function atomForConfig<T extends Record<string, unknown>>(key: string, defaultValue: T) {
+  const storageAtom = atomWithStorage(key, defaultValue)
+  return atom(
+    (get) => {
+      // Get the underlying object
+      const config = get(storageAtom)
+      // Check if there are missing properties
+      let hasMissingProperty = false
+      for (const key in defaultValue) {
+        if (!(key in config)) {
+          hasMissingProperty = true
+        }
+      }
+      // Merge iff there are missing properties
+      return hasMissingProperty ? { ...defaultValue, ...config } : config
+    },
+    (_, set, newConfig: T) => set(storageAtom, newConfig),
+  )
+}
