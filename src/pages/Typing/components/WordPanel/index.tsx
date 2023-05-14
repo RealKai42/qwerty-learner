d8bf2d172f7b4b85aef8257aaee93eade35d23fe
邓亮 <787615673@qq.com>
2023-05-14 15:33:51 +0800

    fix: 少了一个currentWord判断当前是否值

--- src/pages/Typing/components/WordPanel/index.tsx
+++ src/pages/Typing/components/WordPanel/index.tsx
@@ -64,7 +64,7 @@ export default function WordPanel() {
 
   return (
     <div className="flex flex-col items-center">
-      {
+      {currentWord && (
         <>
           <div className="flex items-center">
             <IconBackArrow
@@ -90,7 +90,7 @@ export default function WordPanel() {
             ></IconForwardArrow>
           </div>
         </>
-      }
+      )}
     </div>
   )
 }
