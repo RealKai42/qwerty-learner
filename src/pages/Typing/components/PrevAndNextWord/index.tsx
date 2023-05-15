e6294177ff267e402137882efacbf2e835007920
KaiyiWing <Zhang.kaiyi42@gmail.com>
2023-05-15 15:22:33 +0800

    feat: add hotkey

--- src/pages/Typing/components/PrevAndNextWord/index.tsx
+++ src/pages/Typing/components/PrevAndNextWord/index.tsx
@@ -1,5 +1,7 @@
 import { TypingContext, TypingStateActionType } from '../../store'
+import Tooltip from '@/components/Tooltip'
 import { useContext, useCallback, useMemo } from 'react'
+import { useHotkeys } from 'react-hotkeys-hook'
 import IconPrev from '~icons/tabler/arrow-narrow-left'
 import IconNext from '~icons/tabler/arrow-narrow-right'
 
@@ -9,28 +11,43 @@ export default function PrevAndNextWord({ type }: LastAndNextWordProps) {
 
   const newIndex = useMemo(() => state.chapterData.index + (type === 'prev' ? -1 : 1), [state.chapterData.index, type])
   const word = state.chapterData.words[newIndex]
+  const shortCutKey = useMemo(() => (type === 'prev' ? 'Ctrl + Shift + ArrowLeft' : 'Ctrl + Shift + ArrowRight'), [type])
 
   const onClickWord = useCallback(() => {
+    if (!word) return
+
     if (type === 'prev') dispatch({ type: TypingStateActionType.SKIP_2_WORD_INDEX, newIndex })
     if (type === 'next') dispatch({ type: TypingStateActionType.SKIP_2_WORD_INDEX, newIndex })
-  }, [type, dispatch, newIndex])
+  }, [type, dispatch, newIndex, word])
+
+  useHotkeys(
+    shortCutKey,
+    (e) => {
+      e.preventDefault()
+      onClickWord()
+    },
+    { preventDefault: true },
+  )
 
   return (
     <>
       {word ? (
-        <div
-          onClick={onClickWord}
-          className="flex max-w-xs cursor-pointer select-none items-center text-gray-700 opacity-60 duration-200 ease-in-out hover:opacity-100 dark:text-gray-400"
-        >
-          {type === 'prev' && <IconPrev className="mr-4 shrink-0 grow-0 text-2xl" />}
-          <div className={`grow-1 flex w-full flex-col ${type === 'next' ? 'items-end text-right' : ''}`}>
-            <p className="font-mono text-2xl font-normal text-gray-700 dark:text-gray-400">{word.name}</p>
-            {state.isTransVisible && (
-              <p className="line-clamp-1 max-w-full text-sm font-normal text-gray-600 dark:text-gray-500">{word.trans.join('；')}</p>
-            )}
+        <Tooltip content={`快捷键: ${shortCutKey}`}>
+          <div
+            onClick={onClickWord}
+            className="flex max-w-xs cursor-pointer select-none items-center text-gray-700 opacity-60 duration-200 ease-in-out hover:opacity-100 dark:text-gray-400"
+          >
+            {type === 'prev' && <IconPrev className="mr-4 shrink-0 grow-0 text-2xl" />}
+
+            <div className={`grow-1 flex w-full flex-col ${type === 'next' ? 'items-end text-right' : ''}`}>
+              <p className="font-mono text-2xl font-normal text-gray-700 dark:text-gray-400">{word.name}</p>
+              {state.isTransVisible && (
+                <p className="line-clamp-1 max-w-full text-sm font-normal text-gray-600 dark:text-gray-500">{word.trans.join('；')}</p>
+              )}
+            </div>
+            {type === 'next' && <IconNext className="ml-4 shrink-0 grow-0 text-2xl" />}
           </div>
-          {type === 'next' && <IconNext className="ml-4 shrink-0 grow-0 text-2xl" />}
-        </div>
+        </Tooltip>
       ) : (
         <div />
       )}
