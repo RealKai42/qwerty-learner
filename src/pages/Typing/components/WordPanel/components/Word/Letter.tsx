adf22fd48fbca4c88556d2a9e7da41b1e9cbec62
Lee <43327024+DesnLee@users.noreply.github.com>
2023-05-15 14:11:48 +0800

    feat: add animation when visible changes (#478)

--- src/pages/Typing/components/WordPanel/components/Word/Letter.tsx
+++ src/pages/Typing/components/WordPanel/components/Word/Letter.tsx
@@ -1,3 +1,4 @@
+import s from './Letter.module.css'
 import { EXPLICIT_SPACE } from '@/constants'
 import React from 'react'
 
@@ -23,13 +24,14 @@ export type LetterProps = {
 }
 
 const Letter: React.FC<LetterProps> = ({ letter, state = 'normal', visible = true }) => (
-  //todo: may add animation when visible changes
   <span
-    className={`m-0 p-0 font-mono text-5xl font-normal ${
+    className={`m-0 h-14 overflow-hidden p-0 font-mono text-5xl font-normal ${
       stateClassNameMap[(letter === EXPLICIT_SPACE) as unknown as string][state]
     } pr-0.8 duration-0 dark:text-opacity-80`}
   >
-    {visible ? letter : '_'}
+    <span className={`h-28 ${s.letter} ${visible ? s.letterVisible : s.letterHide}`}>
+      <span>{letter}</span>
+    </span>
   </span>
 )
 
