95e0e0fea2ab7529fa0a6092dc669c343a481a92
DesnLee <jiakun.ui@gmail.com>
2023-05-16 19:48:45 +0800

    fix: fix the issue of text not being centered in translation

--- src/pages/Typing/components/WordPanel/components/Translation/index.tsx
+++ src/pages/Typing/components/WordPanel/components/Translation/index.tsx
@@ -8,7 +8,7 @@ export default function Translation({ trans }: TranslationProps) {
   const isTextSelectable = useAtomValue(isTextSelectableAtom)
   return (
     <div
-      className={`max-w-4xl pb-4 pt-5 font-sans text-lg transition-colors duration-300 dark:text-white dark:text-opacity-80 ${
+      className={`max-w-4xl pb-4 pt-5 text-center font-sans text-lg transition-colors duration-300 dark:text-white dark:text-opacity-80 ${
         !isTextSelectable && 'select-none'
       }`}
     >
