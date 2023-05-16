db0c179d41be5030fb337c770d438c7af92ab234
DesnLee <jiakun.ui@gmail.com>
2023-05-16 19:26:06 +0800

    feat: add a blurred mask during pause and display "继续" instead of "开始".

--- src/pages/Typing/components/WordPanel/index.tsx
+++ src/pages/Typing/components/WordPanel/index.tsx
@@ -8,6 +8,11 @@ import { isShowPrevAndNextWordAtom, phoneticConfigAtom } from '@/store'
 import { useAtomValue } from 'jotai'
 import { useCallback, useContext, useState } from 'react'
 
+const maskBg = {
+  transparent: 'bg-[#faf9ffcc] dark:bg-[#1f1f1fcc]',
+  opaque: 'bg-[#faf9ff] dark:bg-[#1f1f1f]',
+}
+
 export default function WordPanel() {
   // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
   const { state, dispatch } = useContext(TypingContext)!
@@ -47,17 +52,28 @@ export default function WordPanel() {
         )}
       </div>
       <div className="container flex flex-grow flex-col items-center justify-center">
-        {currentWord && state.isTyping ? (
-          <>
-            <WordComponent word={currentWord.name} onFinish={onFinish} key={wordComponentKey} />
-            {phoneticConfig.isOpen && <Phonetic word={currentWord} />}
-            {state.isTransVisible && <Translation trans={currentWord.trans.join('；')} />}
-          </>
-        ) : (
-          <div className="animate-pulse select-none  text-xl text-gray-600 dark:text-gray-50">按任意键开始</div>
+        {currentWord && (
+          <div className="relative flex w-full justify-center">
+            {!state.isTyping && (
+              <div
+                className={`absolute left-0 top-0 z-10 flex h-full w-full items-center backdrop-blur-sm ${
+                  state.chapterData.index === 0 ? maskBg.opaque : maskBg.transparent
+                }`}
+              >
+                <p className="w-full animate-pulse select-none text-center text-3xl text-gray-600 dark:text-gray-50">
+                  按任意键{state.chapterData.index === 0 ? '开始' : '继续'}
+                </p>
+              </div>
+            )}
+            <div className="relative">
+              <WordComponent word={currentWord.name} onFinish={onFinish} key={wordComponentKey} />
+              {phoneticConfig.isOpen && <Phonetic word={currentWord} />}
+              {state.isTransVisible && <Translation trans={currentWord.trans.join('；')} />}
+            </div>
+          </div>
         )}
       </div>
-      {state.isTyping && <Progress className="mb-10 mt-auto" />}
+      <Progress className={`mb-10 mt-auto ${state.isTyping ? 'text-opacity-100' : 'opacity-0'}`} />
     </div>
   )
 }
