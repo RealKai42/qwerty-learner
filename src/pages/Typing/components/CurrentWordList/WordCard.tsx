bf44b0c5bb91eba0ecefd38192e34e238ad9a2e1
KaiyiWing <Zhang.kaiyi42@gmail.com>
2023-05-17 21:41:29 +0800

    style: add dark mode support

--- src/pages/Typing/components/CurrentWordList/WordCard.tsx
+++ src/pages/Typing/components/CurrentWordList/WordCard.tsx
@@ -4,8 +4,8 @@ export default function WordCard({ word, isActive }: { word: Word; isActive: boo
   return (
     <div
       className={`mb-2 rounded-xl ${
-        isActive ? 'bg-indigo-50' : 'bg-white'
-      }   p-4 shadow focus:outline-none dark:border-gray-500 dark:bg-gray-700 dark:bg-opacity-10`}
+        isActive ? 'bg-indigo-50 dark:bg-indigo-800 dark:bg-opacity-20' : 'bg-white dark:bg-gray-700 dark:bg-opacity-20'
+      }   p-4 shadow focus:outline-none `}
       key={word.name}
     >
       <p className="font-mono text-xl font-normal leading-6 dark:text-gray-50">{word.name}</p>
