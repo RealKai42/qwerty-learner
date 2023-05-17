86256ee737f366609328671c3e79b6fd7bba13ac
Lv777 <39904184+OBQun@users.noreply.github.com>
2023-05-18 00:45:34 +0800

    feat: read out translation by web speech api (#409)
    Co-authored-by: KaiyiWing <Zhang.kaiyi42@gmail.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -41,9 +41,13 @@ export const pronunciationConfigAtom = atomForConfig('pronunciation', {
   type: 'us' as PronunciationType,
   name: '美音',
   isLoop: false,
+  isTransRead: false,
+  transVolume: 1,
   rate: 1,
 })
 export const pronunciationIsOpenAtom = atom((get) => get(pronunciationConfigAtom).isOpen)
+
+export const pronunciationIsTransReadAtom = atom((get) => get(pronunciationConfigAtom).isTransRead)
 
 export const randomConfigAtom = atomForConfig('randomConfig', {
   isOpen: false,
