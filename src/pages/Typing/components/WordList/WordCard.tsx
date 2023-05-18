475c5de2b779c8b0678af2c10cb9c059ecb82628
TaksonyL <53998893+TaksonyL@users.noreply.github.com>
2023-05-18 13:50:50 +0800

    style: update user select (#498)
    * update: user-select
    
    * update: sidebar user select

--- src/pages/Typing/components/WordList/WordCard.tsx
+++ src/pages/Typing/components/WordList/WordCard.tsx
@@ -5,10 +5,10 @@ export default function WordCard({ word, isActive }: { word: Word; isActive: boo
     <div
       className={`mb-2 rounded-xl ${
         isActive ? 'bg-indigo-50 dark:bg-indigo-800 dark:bg-opacity-20' : 'bg-white dark:bg-gray-700 dark:bg-opacity-20'
-      }   p-4 shadow focus:outline-none `}
+      }   select-text p-4 shadow focus:outline-none`}
       key={word.name}
     >
-      <p className="font-mono text-xl font-normal leading-6 dark:text-gray-50">{word.name}</p>
+      <p className="select-all font-mono text-xl font-normal leading-6 dark:text-gray-50">{word.name}</p>
       <div className="mt-2 font-sans text-sm text-gray-400">{word.trans}</div>
     </div>
   )
--- src/pages/Typing/components/WordPanel/components/Phonetic/index.tsx
+++ src/pages/Typing/components/WordPanel/components/Phonetic/index.tsx
@@ -13,7 +13,7 @@ function Phonetic({ word }: PhoneticProps) {
   return (
     <div
       className={`space-x-5 pt-1 text-center text-sm font-normal text-gray-600 transition-colors duration-300 dark:text-gray-400 ${
-        !isTextSelectable && 'select-none'
+        isTextSelectable && 'select-text'
       }`}
     >
       {phoneticConfig.type === 'us' && word.usphone && word.usphone.length > 1 && <span>{`AmE: [${word.usphone}]`}</span>}
