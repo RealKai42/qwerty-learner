9d8faaa4c6275d0c876467e8d7d659eb97669ba0
Jianbo.Yu <2515100339@qq.com>
2023-11-01 16:58:37 +0800

    feat: 拼写界面展示当前进度

--- src/pages/Typing/components/Speed/index.tsx
+++ src/pages/Typing/components/Speed/index.tsx
@@ -10,10 +10,13 @@ export default function Speed() {
   const secondsString = seconds < 10 ? '0' + seconds : seconds + ''
   const minutesString = minutes < 10 ? '0' + minutes : minutes + ''
   const inputNumber = state.chapterData.correctCount + state.chapterData.wrongCount
+  const wordCount = state.chapterData.wordCount
+  const totalWord = state.chapterData.words.length
 
   return (
     <div className="my-card flex w-3/5 rounded-xl bg-white p-4 py-10 opacity-50 transition-colors duration-300 dark:bg-gray-800">
       <InfoBox info={`${minutesString}:${secondsString}`} description="时间" />
+      <InfoBox info={`${wordCount}/${totalWord}`} description="进度" />
       <InfoBox info={inputNumber + ''} description="输入数" />
       <InfoBox info={state.timerData.wpm + ''} description="WPM" />
       <InfoBox info={state.chapterData.correctCount + ''} description="正确数" />
