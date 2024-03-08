64297f488745014e1d75d22d4403f7df2f53bba7
Anyexyz <anyexyz@foxmail.com>
2024-03-08 15:30:09 +0800

    Remove Footer component from Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex h-screen w-full flex-col items-center pb-4">
       {children}
-      <Footer />
     </main>
   )
 }
