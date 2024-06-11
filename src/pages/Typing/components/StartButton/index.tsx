33d1f3652d102f447f357b8f72f459e132e3ef14
Jay Chou <84218188+JayChou404@users.noreply.github.com>
2024-06-11 17:00:52 +0800

    【优化】简单优化了一下StartButton按钮的逻辑 (#788)
    * 发现一个提示窗bug
    
    * 优化startbutton提示框提醒，统一风格：[功能] （keymap）
    
    * fix: remove comments
    
    ---------
    
    Co-authored-by: Kai <me@kaiyi.cool>

--- src/pages/Typing/components/StartButton/index.tsx
+++ src/pages/Typing/components/StartButton/index.tsx
@@ -32,7 +32,7 @@ export default function StartButton({ isLoading }: { isLoading: boolean }) {
   const { getReferenceProps, getFloatingProps } = useInteractions([hoverButton])
 
   return (
-    <Tooltip content="快捷键 Enter" className="box-content h-7 w-8 px-6 py-1">
+    <Tooltip content={`${state.isTyping ? '暂停' : '开始'} （Enter）`} className="box-content h-7 w-8 px-6 py-1">
       <div
         ref={refs.setReference}
         {...getReferenceProps()}
