bc5bfab9232003a20f4ca25c560937fce476b6a9
boy <101509477+rainnoon@users.noreply.github.com>
2024-09-06 11:35:04 +0800

    feat: "add svg bg"

---
+++ src/pages/Mobile/index.tsx
@@ -0,0 +1,12 @@
+import Flow from './flow'
+import type React from 'react'
+
+const MobilePage: React.FC = () => {
+  return (
+    <div className="flex h-screen w-screen  ">
+      <Flow />
+    </div>
+  )
+}
+
+export default MobilePage
