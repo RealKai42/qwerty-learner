216b2e9b63d2e7209f424b542d939a3fc94f9487
Shine-hao <haozschn@mail.ru>
2026-05-29 10:25:58 +0800

    Update dictionary.ts

--- src/resources/dictionary.ts
+++ src/resources/dictionary.ts
@@ -4187,15 +4187,15 @@ export const dictionaryResources: DictionaryResource[] = [
   //   length: 20,
   //   language: 'ja',
   // },
-   {
+    {
     id: "russian_1000",
-    name: "俄语常用 1000 词",
+    name: "俄语常用1000词",
     description: "俄语常用基础词汇，适合初学者",
     category: "俄语",
     url: "/dicts/russian.json",
-    length: 1000,
+    length: 4,
     language: "ru"
-  } ,
+  },
 ]
 export const dictionaries: Dictionary[] = dictionaryResources.map((resource) => ({
   ...resource,
